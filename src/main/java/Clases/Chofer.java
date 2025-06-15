67f56e3f1236117f4eb90a7d00fae8c1995b95a8
Ezee-79 <gezromerosuju@gmail.com>
2025-06-15 15:59:22 -0300

    Creacion de Enum Categoria,Provincia y Clase Chofer
    Creacion de Enum Categoria,Provincia y creacion de clase Chofer

--- src/main/java/Clases/Chofer.java
+++ src/main/java/Clases/Chofer.java
@@ -9,5 +9,29 @@ package Clases;
  * @author Gaston PC
  */
 public class Chofer {
+    private String nroLicencia;
+    private Categoria categoria;
     
+    public Chofer(String nroLicencia, Categoria categoria){
+        this.nroLicencia = nroLicencia;
+        this.categoria = categoria;
+    }
+    
+    // set
+    public void setNroLicencia( String nroLicencia){
+        this.nroLicencia= nroLicencia;
+    }
+    public void setCategoria( Categoria categoria){
+        this.categoria= categoria;
+    }
+    
+    // gett
+    
+    public String getNroLicencia(){
+        return nroLicencia;
+    }
+    public Categoria getCategoria(){
+        return categoria;
+    }
+
 }
