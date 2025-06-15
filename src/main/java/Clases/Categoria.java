f82d5af8c574b99ef6857c7e04c3d4b373a9ede1
Ezee-79 <gezromerosuju@gmail.com>
2025-06-15 19:43:06 -0300

    Actualizacion Clase categoria
    Actualizacion Clase categoria

--- src/main/java/Clases/Categoria.java
+++ src/main/java/Clases/Categoria.java
@@ -1,5 +1,23 @@
-package Clases;
+
+ package Clases;
 
 public class Categoria {
+    private NCategoria tipo;
 
+    public Categoria (NCategoria tipo) {
+        this.tipo = tipo;
+    }
+
+    //set
+
+    public void setTipo(NCategoria tipo) {
+        this.tipo = tipo;
+    }
+
+    //get
+    public NCategoria getTipo() {
+        return tipo;
+    }
 }
+
+
