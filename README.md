9dec495ba75d26f50b9366ee166bb9a48ab14788
Ezee-79 <gezromerosuju@gmail.com>
2025-06-15 15:02:35 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Taller_de_Poo_2025
+Taller Poo 2025 
---
+++ src/main/java/com/mycompany/taller_de_poo_2025/Taller_de_Poo_2025.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.taller_de_poo_2025;
+
+/**
+ *
+ * @author Gaston PC
+ */
+public class Taller_de_Poo_2025 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
