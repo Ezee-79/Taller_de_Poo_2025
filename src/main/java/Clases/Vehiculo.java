fe28313f8a0a3ca320d00326a63a9fd5a1eddee9
EnzoPor98 <127218831+EnzoPor98@users.noreply.github.com>
2025-06-15 17:00:27 -0300

    CreacionClases
    Ciudad, colectivo, minibus y vehiculo.

---
+++ src/main/java/Clases/Vehiculo.java
@@ -0,0 +1,30 @@
+package Clases;
+
+public class Vehiculo {
+    private String patente;
+    private int capacidad;
+
+    public Vehiculo() {
+    }
+
+    public Vehiculo(String patente, int capacidad) {
+        this.patente = patente;
+        this.capacidad = capacidad;
+    }
+
+    public String getPatente() {
+        return patente;
+    }
+
+    public void setPatente(String patente) {
+        this.patente = patente;
+    }
+
+    public int getCapacidad() {
+        return capacidad;
+    }
+
+    public void setCapacidad(int capacidad) {
+        this.capacidad = capacidad;
+    }
+}
