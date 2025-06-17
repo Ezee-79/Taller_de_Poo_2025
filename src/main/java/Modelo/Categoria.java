c0196cf23b4f6e909bc254424397c486227739ef
Sined108 <denistaejoo1@gmail.com>
2025-06-16 23:19:59 -0300

    Agregacion de relaciones
    Agregando las relaciones de cada clase

--- src/main/java/Modelo/Categoria.java
+++ src/main/java/Modelo/Categoria.java
@@ -1,5 +1,20 @@
 package Modelo;
 
 public class Categoria {
+    private EnumCategoria tipo;
 
+    public Categoria (EnumCategoria tipo) {
+        this.tipo = tipo;
+    }
+
+    //set
+
+    public void setTipo(EnumCategoria tipo) {
+        this.tipo = tipo;
+    }
+
+    //get
+    public EnumCategoria getTipo() {
+        return tipo;
+    }
 }
--- src/main/java/Modelo/Chofer.java
+++ src/main/java/Modelo/Chofer.java
@@ -2,32 +2,53 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Modelo;
-
+ 
+ package Modelo;
+ import java.util.ArrayList;
 /**
  *
  * @author Gaston PC
  */
 public class Chofer extends Persona {
     private String nroLicencia;
+    private Viaje viaje;
+    private ArrayList <ChoferCategoria> choferCategorias;
 
-    public Chofer() {
-    }
-
-    public Chofer(String nroLicencia) {
+    
+    public Chofer(String nroLicencia, long dni, String nombre, String apellido, Viaje viaje){
+        super(dni, nombre, apellido);
         this.nroLicencia = nroLicencia;
-
+        this.viaje = viaje;
+        this.choferCategorias = new ArrayList<ChoferCategoria>();
     }
-
+    
     // set
-    public void setNroLicencia(String nroLicencia) {
-        this.nroLicencia = nroLicencia;
+    public void setNroLicencia(String nroLicencia){
+        this.nroLicencia= nroLicencia;
     }
 
-    // gett
+    public void setViaje (Viaje viaje) {
+        this.viaje = viaje;
+    }
 
-    public String getNroLicencia() {
+    public void setChoferCategoria(ArrayList <ChoferCategoria> choferCategorias) {
+        this.choferCategorias = new ArrayList<ChoferCategoria>();
+    }
+   
+    
+    // get
+    
+    public String getNroLicencia(){
         return nroLicencia;
     }
+
+    public Viaje getViaje(){
+        return viaje;
+    }
+
+    public ArrayList <ChoferCategoria> getChoferCategorias(){
+        return choferCategorias;
+    }
+ 
 
 }
