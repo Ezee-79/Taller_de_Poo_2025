42ad2a2584664f987702f58d09cb3c6954fce22e
EnzoPor98 <127218831+EnzoPor98@users.noreply.github.com>
2025-06-20 03:32:23 -0300

    Estructura del Menu
    Menu con submenus, idea inicial.

--- src/main/java/com/mycompany/Vista/SistemaGestion.java
+++ src/main/java/com/mycompany/Vista/SistemaGestion.java
@@ -4,14 +4,13 @@
  */
 package com.mycompany.Vista;
 import java.util.*;
-import Modelo.Viaje;
 /**
  *
  * @author Gaston PC
  */
 public class SistemaGestion {
    
-    private ArrayList<Viaje> viajes = new ArrayList<>();
+    //private ArrayList<Viaje> viajes = new ArrayList<>();
     
     
     public void registrarChofer(Scanner sc) {
